56e5302a36cb8a317e7b8f0566d2473c14032d1f
jonathan zhang <jonathanz28@nycstudents.net>
2022-11-10 09:21:12 -0500

    ZhangU4T1Lab2

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,32 @@
-package PACKAGE_NAME;public class Main {
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a positive, even number that is divisible by 7: ");
+        int userNumber = scanner.nextInt();
+        boolean not = true;
+        boolean found = false;
+
+        while (true) {
+            if (userNumber > 0 && userNumber % 2 == 0 && userNumber % 7 == 0) {
+                not = false;
+            } else {
+                if (userNumber <= 0) {
+                    System.out.println("Not positive");
+                }
+                if (userNumber % 2 != 0) {
+                    System.out.println("Not even");
+                }
+                if (userNumber % 7 != 0) {
+                    System.out.println("Not divisible by 7");
+                }
+                System.out.println("Try Again: ");
+                userNumber = scanner.nextInt();
+            }
+        }
+
+        System.out.println("Thank you, goodbye!");
+    }
 }
+
